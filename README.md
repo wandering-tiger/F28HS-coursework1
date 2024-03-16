80752b67d7302262142d0f6141f1f13a710cb5e9
fzb <2368376942@qq.com>
2024-03-16 11:28:53 +0800

    submission without Bonus task

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # F28HS Coursework 1
 
-(Delete this line, and add the list of your tasks and how to compile and run your program here.)
-unsigned int
+## My task
+I found my task in task list.
+> H00391678: HSHEX NOISE COMP
+
+## How to run my program
+Firstly, use `make` to compile my program, since a Makefile is provided.<br>
+Secondly, run it from the command-line. Use arguments like below.<br>
+> `./process reference.image input.image output.image noise_strength`
+
+## Question 1 and 2
+I didn't add any parameters.
+
+## Question 3(NOISE)
+My NOISE function process the `input.image`, and save the processed image in `output.image`.<br>
+Also, a `noise_strength` parameter is added in the end.
+
+## Question 4(COMP)
+My COMP function compare the `reference.image` and `input.image`.
+
+## Question 5
--- process.c
+++ process.c
@@ -29,12 +29,26 @@ struct Image {
 void free_image(struct Image *img)
 {
     /* TODO: Question 2a */
-    for(int i=0;i<img->width;i++){
-        for(int j=0;j<img->height;j++){
-            free(&img->pixel[i][j]);
-            // free(*(img->pixel+i)+j);
-        }
+    // for(int i=0;i<img->width;i++){
+    //     for(int j=0;j<img->height;j++){
+    //         free(&img->pixel[i][j]);
+    //         // free(*(img->pixel+i)+j);
+    //     }
+    // }
+    // free(img);
+    if (img == NULL) {
+        return; // Nothing to free if the image is NULL
     }
+    // Free the memory for pixel array
+    if (img->pixel != NULL) {
+        // Free each row of pixel array
+        for (int i = 0; i < img->height; i++) {
+            free(img->pixel[i]);
+        }
+        // Free the array of row pointers
+        free(img->pixel);
+    }
+    // Free the struct Image itself
     free(img);
 }
 
@@ -103,7 +117,7 @@ bool save_image(const struct Image *img, const char *filename)
     // write the image
     for(int i=0;i<img->height;i++){
         for(int j=0;j<img->width;j++){
-            if(fprintf(f,"%x %x %x",img->pixel[i][j].red,img->pixel[i][j].green,img->pixel[i][j].blue)==EOF){
+            if(fprintf(f,"%x %x %x ",img->pixel[i][j].red,img->pixel[i][j].green,img->pixel[i][j].blue)==EOF){
                 printf("write error\n");
                 return false;
             }
@@ -118,28 +132,95 @@ bool save_image(const struct Image *img, const char *filename)
 struct Image *copy_image(const struct Image *source)
 {
     /* TODO: Question 2d */
-    
-    return NULL;
+    // allocate struct Image
+    struct Image *img = (struct Image*)malloc(sizeof(struct Image));
+    img->width=source->width;
+    img->height=source->height;
+    img->pixel=(struct Pixel**)malloc(sizeof(struct Pixel*)*img->height);
+    for (int i=0; i<img->height;i++) {
+        img->pixel[i] = (struct Pixel*)malloc(sizeof(struct Pixel)*img->width);
+    }
+
+    // copy the image ? not sure about how to copy const struct
+    for(int i=0;i<img->height;i++){
+        for(int j=0;j<img->width;j++){
+            img->pixel[i][j]=source->pixel[i][j];
+        }
+    }
+    return img;
 }
 
 /* Perform your first task.
  * (TODO: Write a better comment here, and rename the function.
  * You may need to add or change arguments depending on the task.)
  * Returns a new struct Image containing the result, or NULL on error. */
-struct Image *apply_BLUR(const struct Image *source)
+struct Image *apply_NOISE(const struct Image *source, const char *ns)
 {
     /* TODO: Question 3 */
-    return NULL;
+    // create a copy of original image
+    struct Image *img = (struct Image*)malloc(sizeof(struct Image));
+    img=copy_image(source);
+    // generate the rand seed
+    time_t t;
+    srand((unsigned) time(&t));
+    // convert strength to int
+    int strength=atoi(ns);
+    if(strength==0){
+        return img;
+    }
+    // add the noise
+    int temp;
+    int divisor=strength*2+1;
+    for(int i=0;i<img->height;i++){
+        for(int j=0;j<img->width;j++){
+            temp = (rand() % divisor)-strength;
+            // avoid integer wraparound
+            if(img->pixel[i][j].red+temp<=0xffff && img->pixel[i][j].red+temp>=0){
+                img->pixel[i][j].red+=temp;
+            }
+            temp = (rand() % divisor)-strength;
+            if(img->pixel[i][j].green+temp<=0xffff && img->pixel[i][j].green+temp>=0){
+                img->pixel[i][j].green+=temp;
+            }
+            temp = (rand() % divisor)-strength;
+            if(img->pixel[i][j].blue+temp<=0xffff && img->pixel[i][j].blue+temp>=0){
+                img->pixel[i][j].blue+=temp;
+            }
+        }
+    }
+    return img;
 }
 
 /* Perform your second task.
  * (TODO: Write a better comment here, and rename the function.
  * You may need to add or change arguments depending on the task.)
  * Returns true on success, or false on error. */
-bool apply_NORM(const struct Image *source)
+bool apply_COMP(const struct Image *source, const struct Image *reference)
 {
     /* TODO: Question 4 */
-    return false;
+    if(source==NULL || reference==NULL){
+        return false;
+    }
+    int identical=0,different=0;
+    int coincident_width, coincident_height;
+    // calculate the coincident part
+    coincident_width= source->width > reference->width ? reference->width : source->width;
+    coincident_height= source->height > reference->height ? reference->height : source->height;
+    // count the same pixels
+    for(int i=0;i<coincident_height;i++){
+        for(int j=0;j<coincident_width;j++){
+            if(source->pixel[i][j].red==reference->pixel[i][j].red && source->pixel[i][j].green==reference->pixel[i][j].green && source->pixel[i][j].blue==reference->pixel[i][j].blue){
+                identical++;
+            }
+        }
+    }
+    // count the diffrent pixels
+    int total=source->width*source->height + reference->width*reference->height;
+    different=total-2*identical;
+
+    printf("Identical pixels: %d\n",identical);
+    printf("Different pixels: %d\n",different);
+    return true;
 }
 
 int main(int argc, char *argv[])
@@ -149,25 +230,33 @@ int main(int argc, char *argv[])
     srand(time(NULL));
 
     /* Check command-line arguments */
-    if (argc != 3) {
+    // modify the argc number for noise task
+    if (argc != 5) {
         fprintf(stderr, "Usage: process INPUTFILE OUTPUTFILE\n");
         return 1;
     }
 
     /* Load the input image */
-    struct Image *in_img = load_image(argv[1]);
+    struct Image *in_img = load_image(argv[2]);
     if (in_img == NULL) {
         return 1;
     }
 
     /* Test of saving the input image */
-    if (!save_image(in_img, argv[2])) {
-        fprintf(stderr, "Saving image to %s failed.\n", argv[2]);
-        return 1;
-    }
+    // if (!save_image(in_img, argv[2])) {
+    //     fprintf(stderr, "Saving image to %s failed.\n", argv[2]);
+    //     return 1;
+    // }
+
+    /* Test of copying the input image */
+    // struct Image *copy_img = copy_image(in_img);
+    // if (!save_image(copy_img, argv[2])) {
+    //     fprintf(stderr, "Copying image to %s failed.\n", argv[2]);
+    //     return 1;
+    // }
 
     /* Apply the first process */
-    struct Image *out_img = apply_BLUR(in_img);
+    struct Image *out_img = apply_NOISE(in_img,argv[4]);
     if (out_img == NULL) {
         fprintf(stderr, "First process failed.\n");
         free_image(in_img);
@@ -175,16 +264,18 @@ int main(int argc, char *argv[])
     }
 
     /* Apply the second process */
-    if (!apply_NORM(out_img)) {
+    struct Image *reference_img = load_image(argv[1]);
+    if (!apply_COMP(in_img,reference_img)) {
         fprintf(stderr, "Second process failed.\n");
         free_image(in_img);
         free_image(out_img);
         return 1;
     }
+    free_image(reference_img);
 
     /* Save the output image */
-    if (!save_image(out_img, argv[2])) {
-        fprintf(stderr, "Saving image to %s failed.\n", argv[2]);
+    if (!save_image(out_img, argv[3])) {
+        fprintf(stderr, "Saving image to %s failed.\n", argv[3]);
         free_image(in_img);
         free_image(out_img);
         return 1;
